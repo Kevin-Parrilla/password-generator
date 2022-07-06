759b26b05bdea6c2260baa4dcb7d94ae2af91500
Kevin <kevinp.codes@gmail.com>
2022-07-06 16:18:44 -0400

    update password generator

--- password-generator.py
+++ password-generator.py
@@ -1,7 +1,7 @@
+from enum import auto
 import string, random
 
 auto_gen_pass = []
-auto_gen_pass_count = len(auto_gen_pass) 
 
 for i in range(5):
     auto_gen_pass.append(random.choice(string.ascii_letters))
